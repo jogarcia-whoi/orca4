652843daee00bb49da7c637ba281b7f164d63f8b
Nathan Figueroa <nathan.figueroa@whoi.edu>
2024-07-03 12:52:33 -0400

    test: test docstrings. Added pytest mock to image

--- orca_extend/test/test_pep257.py
+++ orca_extend/test/test_pep257.py
@@ -19,5 +19,6 @@ import pytest
 @pytest.mark.linter
 @pytest.mark.pep257
 def test_pep257():
+    ''' Tests PEP257 compliance, e.g. docstring conventions. Remove if not wanted. '''
     rc = main(argv=['.', 'test'])
     assert rc == 0, 'Found code style errors / warnings'
